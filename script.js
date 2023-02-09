15aad256d8fcfc44351d09b86b960d398751b5ab
bbetwo <103236521+bbetwo@users.noreply.github.com>
2023-02-09 07:39:58 +0300

    Test

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const bg = document.querySelector(".bg");
 const loadText = document.querySelector(".loading-text");
 
+const btn = document.getElementById('btn')
+
 let timer = 0;
 
-let int = setInterval(blur, 30);
+
 function blur() {
   timer++;
   if (timer >= 100) {
@@ -19,3 +21,7 @@ function blur() {
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
+btn.addEventListener('click', ()=>{
+  setInterval(blur, 30);
+})
